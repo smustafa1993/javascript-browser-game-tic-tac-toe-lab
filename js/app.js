0e0bdd17069fa4dd7c0e3e003bfe2e88b7d0a3c6
smustafa1993 <sayedalsharaf@gmail.com>
2025-02-26 03:36:32 +0000

    logic works

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let turn;
 let winner;
 let tie;
 let starter="X";
+let x=[];
+let o=[];
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -21,6 +23,29 @@ function init(){}
 function ClickPlace(){}
 function handleClick(){}
 
+function addIndex(index,value){
+
+    if(value=="X"){
+        x.push(index);
+    }
+    else o.push(index);
+
+    console.log(`X has: ${x}, and O has: ${o}`)
+
+}
+function checkWin(){
+
+    x.sort((a,b)=>a-b);
+    o.sort((a,b)=>a-b);
+
+    const areArraysEqual = x.length === winCondition[0].length && x.every((value, index) => value ===winCondition[0][index]);
+    if (areArraysEqual) {
+        console.log('X WON!!! WOOOHOOOO');
+      }
+
+
+}
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -31,14 +56,20 @@ squareEls.forEach((square,index)=>{
 
     square.addEventListener('click',()=>{
         if(starter==="X"){
-        square.textContent="X";
+           
+        square.textContent=starter;
+        addIndex(index,starter);
         starter="O";
+        
     }
         else{
+     
             square.textContent=starter;
+            addIndex(index,starter);
             starter="X";
         }
         console.log("Clicked!");
+        checkWin();
 
     });    
 
