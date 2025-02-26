f4236ce0eb5d139a41ab4497572520a7f99c7925
smustafa1993 <sayedalsharaf@gmail.com>
2025-02-26 07:09:36 +0000

    slightly buggy, works tho

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
 const winCondition=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-const a=0;
+const message=document.querySelector("#message");
+const resetButton=document.querySelector(".resetButton");
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board;
-let turn;
-let winner;
-let tie;
+let board=0;
 let starter="X";
+let turn=`It's ${starter}'s turn now`;
+message.textContent=turn;
+let winner;
+
 let x=[];
 let o=[];
+
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -19,9 +22,6 @@ const squareEls=document.querySelectorAll(".sqr");
 
 
 /*-------------------------------- Functions --------------------------------*/
-function init(){}
-function ClickPlace(){}
-function handleClick(){}
 
 function addIndex(index,value){
 
@@ -34,14 +34,59 @@ function addIndex(index,value){
 
 }
 function checkWin(){
+    
+    // x.sort((a,b)=>a-b);
+    // o.sort((a,b)=>a-b);
 
-    x.sort((a,b)=>a-b);
-    o.sort((a,b)=>a-b);
+    let xWon=0;
+    let oWon=0;
+    for (let i=0;i<winCondition.length;i++){
+        
+        for (let j=0;j<x.length;j++){
+            if (x[j]===winCondition[i][j]){xWon++;}
+            if(xWon==3){winner="X";break; }
+        }        xWon=0;
 
-    const areArraysEqual = x.length === winCondition[0].length && x.every((value, index) => value ===winCondition[0][index]);
-    if (areArraysEqual) {
-        console.log('X WON!!! WOOOHOOOO');
-      }
+
+        for (let j=0;j<o.length;j++){
+            if (o[j]===winCondition[i][j]){oWon++;}
+            if(oWon==3){winner="O";break; }
+        }        oWon=0;
+        if(xWon>2){break;}
+        if(oWon>2){break;}
+        if(winner)break;
+        if(board>8){console.log("DRAW!");message.textContent="It's a draw!"}
+
+
+
+    }
+    
+
+
+    if(winner){console.log(winner+" won");
+        message.textContent="The winner is "+winner;
+
+    }
+    //else if(oWon==3){console.log(winner+" won");message.textContent="The winner is O!";}
+
+
+}
+
+function reset(){
+    squareEls.forEach((square,index)=>{
+        square.textContent="";
+        });
+         board=0;
+ starter="X";
+ turn=`It's ${starter}'s turn now`;
+message.textContent=turn;
+ winner="";
+
+ x=[];
+ o=[];
+ console.log("reset")
+
+
 
 
 }
@@ -51,29 +96,43 @@ function checkWin(){
 /*----------------------------- Event Listeners -----------------------------*/
 
 
-//document.querySelector(".sqr").addEventListener("click",()=>{console.log("Clk");})
 squareEls.forEach((square,index)=>{
 
     square.addEventListener('click',()=>{
+        
+
         if(starter==="X"){
-           
+            if(square.textContent==""&&!winner){
         square.textContent=starter;
         addIndex(index,starter);
         starter="O";
+        board++;
+        turn=`It's ${starter}'s turn now`;
+        message.textContent=turn;
+        //square.setAttribute("class","sqr2");
+            }
         
     }
         else{
-     
+            if(square.textContent==""&&!winner){
             square.textContent=starter;
             addIndex(index,starter);
             starter="X";
+            board++;
+            turn=`It's ${starter}'s turn now`;
+            message.textContent=turn;
+   }
         }
         console.log("Clicked!");
-        checkWin();
+               checkWin();
+
+               
 
     });    
 
 });
+resetButton.addEventListener('click',reset);
+
 
 
 
