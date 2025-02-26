67fd4bc24e4128f819b4a9e20796977370042823
smustafa1993 <sayedalsharaf@gmail.com>
2025-02-26 02:29:36 +0000

    testing push

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@ const winCondition=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 const a=0;
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board;
+let turn;
+let winner;
+let tie;
 
 
 /*------------------------ Cached Element References ------------------------*/
+// const squareEls;
+// const messageEl;
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init(){}
 
 
 
